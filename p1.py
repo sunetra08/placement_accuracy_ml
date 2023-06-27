d036539d0cb35068488ea80775c19498a6007fb9
Your Name <you@example.com>
2023-06-27 15:58:51 +0530

    1

--- p1.py
+++ p1.py
@@ -40,26 +40,26 @@ accuracy = accuracy_score(y_test, prediction)
 print("Accuracy:", accuracy)
 
 
-# gender_input = int(
-#     input("Enter gender (for male enter 0 for female enter 1): "))
-# ssc_input = float(input("Enter SSC percentage: "))
-# hsc_input = float(input("Enter HSC percentage: "))
-# degree_input = float(input("Enter degree percentage: "))
-# mba_input = float(input("Enter MBA percentage: "))
-# etest_input = float(input("Enter employment test percentage: "))
+gender_input = int(
+    input("Enter gender (for male enter 0 for female enter 1): "))
+ssc_input = float(input("Enter SSC percentage: "))
+hsc_input = float(input("Enter HSC percentage: "))
+degree_input = float(input("Enter degree percentage: "))
+mba_input = float(input("Enter MBA percentage: "))
+etest_input = float(input("Enter employment test percentage: "))
 
-# # Create a new data point using the user input
-# new_data = pd.DataFrame({'gender': [gender_input],
-#                          'ssc_p': [ssc_input],
-#                          'hsc_p': [hsc_input],
-#                          'degree_p': [degree_input],
-#                          'mba_p': [mba_input],
-#                          'etest_p': [etest_input]})
+# Create a new data point using the user input
+new_data = pd.DataFrame({'gender': [gender_input],
+                         'ssc_p': [ssc_input],
+                         'hsc_p': [hsc_input],
+                         'degree_p': [degree_input],
+                         'mba_p': [mba_input],
+                         'etest_p': [etest_input]})
 
 
-# new_prediction = logistic.predict(new_data)
+new_prediction = logistic.predict(new_data)
 
-# print("Prediction for the new data point:", new_prediction)
+print("Prediction for the new data point:", new_prediction)
 
 with open('placement_prediction.pkl', 'wb') as file:
     pickle.dump(logistic, file)
